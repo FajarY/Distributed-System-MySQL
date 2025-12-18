92986f35ee4aab8e23a944fac7388881b3959141
FajarY <fajaryasodana.dev@gmail.com>
2025-12-18 09:07:18 +0700

    feat: Added more lag utils

--- scenario-1/helper-temp.sh
+++ scenario-1/helper-temp.sh
@@ -7,6 +7,9 @@ iperf3 -c 192.168.100.2 -u -b 10G
 #Lag simulation
 tc qdisc add dev eth1 root netem loss 40%
 tc qdisc del dev eth1 root
+tc qdisc add dev eth1 root netem delay 100ms loss 40%
+tc qdisc add dev eth1 root handle 1: tbf rate 2mbit burst 32kbit latency 100ms
+tc qdisc add dev eth1 parent 1:1 handle 10: netem delay 100ms loss 1%
 
 #Bridge utils
 brctl show
--- scenario-1/simulate-lag.sh
+++ scenario-1/simulate-lag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Run in bridge
-tc qdisc add dev eth1 root netem loss 40%
+tc qdisc add dev eth1 root netem delay 100ms loss 40%
